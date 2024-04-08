2d002403f0e59199c4acf01b5ad357fd9665dbf2
EdwardHerr <edrey661@Gmail.com>
2024-04-07 19:12:56 -0700

    Fixing conflicts

--- RandomProgram.java
+++ RandomProgram.java
@@ -3,6 +3,14 @@ class GitAssignment {
     public static void main(String[] args) {
         System.out.println("Hello, Git"); 
 
+        int arr[] = { 12, 3, 4, 15 };
+
+        Arrays.sort(arr);
+        System.out.println("\nThe sorted version of the array is: ");
+        for (int num : arr) {
+            System.out.println(num + " ");
+        }
+
         String randomWords[] = { "lebron", "test", "amazing", "two words" };
 
         Arrays.sort(randomWords);
