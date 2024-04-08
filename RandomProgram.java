902636cd174199c2a6a924fd355d923b7749cf72
EdwardHerr <edrey661@Gmail.com>
2024-04-07 19:00:21 -0700

    Sorting of array for strings featureTwo

--- RandomProgram.java
+++ RandomProgram.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Git"); 
+
+        
+        String randomWords[] = { "lebron", "test", "amazing", "two words" };
+
+        Arrays.sort(randomWords);
+        System.out.println("\nThe sorted version of the string array is: ");
+        for (String list: randomWords) {
+            System.out.println(list + " ");
+        }
+ 
     }
 }
